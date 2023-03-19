895c7611a1505c20095c188149c7b9965af57c45
vladxdboi <vladiks232@inbox.lv>
2023-03-19 19:13:50 +0200

    Lode (kiegelu rupnica)

--- main.py
+++ main.py
@@ -2,30 +2,28 @@
 
 def parallel_processing(n, m, data):
     output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
-
+    next_t = [0] * n
+    av_threads = list(range(n))
+    for indx, thread in enumerate(data):
+        next_thrd = min(av_threads, key=lambda i: next_t[i])
+        output.append((next_thrd, next_t[next_thrd]))
+        next_t[next_thrd] += thread
+        if next_t[next_thrd] == next_t[next_thrd-1]:
+            av_threads.pop(av_threads.index(next_thrd))
+            av_threads.append(next_thrd)
+        
     return output
 
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
-    n = 0
-    m = 0
+    int n, m = 0
+    n, m = map(int, input().split())
+    data = list(map(int, input().split()))
 
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+    result = parallel_processing(n, m, data)
 
-    # TODO: create the function
-    result = parallel_processing(n,m,data)
-    
-    # TODO: print out the results, each pair in it's own line
-
-
+    print("Job schedule:")
+    for i, (thread_num, start_time) in result:
+        print(thread_num,start_time)
 
 if __name__ == "__main__":
     main()
